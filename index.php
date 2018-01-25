05a924890c5f09c5d6705ef07e6009811cad70fd
GauthierBosson <32874699+GauthierBosson@users.noreply.github.com>
2018-01-25 11:08:42 +0100

    Copie nfactoryblog

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+include_once ("./functions/callPage.php");
+include_once("./functions/connectionPDO.php");
+?>
+<?php
+if (isset($_COOKIE['visite'])) {
+    setCookie('visite', $_COOKIE['visite'] + 1 ,time() + 365*24*3600);
+
+} else {
+    setCookie('visite', 1 ,time() + 365*24*3600);
+
+}
+?>
+<?php
+
+echo ( "Ceci est votre ". $_COOKIE['visite'] ." visite !");
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="./assets/css/styles.css" type="text/css" rel="stylesheet" />
+
+    <title>Blog</title>
+
+    <script src="./assets/js/function.js"></script>
+    <script type="text/javascript" src="./assets/js/tinymce/tinymce.min.js"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector: 'textarea'
+        });
+    </script>
+</head>
+<body>
+<div id="container">
+    <?php include_once("./include/header.php");?>
+    <main>
+        <?php
+        callPage();
+        ?>
+    </main>
+    <?php include_once("./include/footer.php");?>
+</div>
+</body>
+</html>
