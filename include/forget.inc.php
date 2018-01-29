1f40861305cdff7bb0b7f81033368c93d3718730
GauthierBosson <32874699+GauthierBosson@users.noreply.github.com>
2018-01-29 10:59:03 +0100

    modif oublie mdp

--- include/forget.inc.php
+++ include/forget.inc.php
@@ -44,6 +44,46 @@ if(isset($_POST['forget'])) {
                         $recupInsert->execute(array($recupMail,$recupCode));
                     }
 
+                    $header="MIME-Version: 1.0\r\n";
+                    $header.='From:"Test.com"<support@test.com>'."\n";
+                    $header.='Content-Type:text/html; charset="utf-8"'."\n";
+                    $header.='Content-Transfer-Encoding: 8bit';
+
+                    $message = '
+                     <html>
+                     <head>
+                       <title>Récupération de mot de passe - Test.com</title>
+                       <meta charset="utf-8" />
+                     </head>
+                     <body>
+                       <font color="#303030";>
+                         <div align="center">
+                           <table width="600px">
+                             <tr>
+                               <td>
+                                 
+                                 <div align="center">Bonjour <b>'.$pseudo.'</b>,</div>
+                                 Voici votre code de récupération: <b>'.$recup_code.'</b>
+                                 A bientôt sur <a href="http://primfx.com/">PrimFX.com</a> !
+                                 
+                               </td>
+                             </tr>
+                             <tr>
+                               <td align="center">
+                                 <font size="2">
+                                   Ceci est un email automatique, merci de ne pas y répondre
+                                 </font>
+                               </td>
+                             </tr>
+                           </table>
+                         </div>
+                       </font>
+                     </body>
+                     </html>
+                     ';
+                    mail($recupMailmail, "Récupération de mot de passe - test.com", $message, $header);
+
+
                     //$recupInsert = $db->prepare('INSERT INTO recuperation(mail,code) VALUES (?,?)');
                     //$recupInsert->execute(array($recupMail, $recupCode));
                 } else {
